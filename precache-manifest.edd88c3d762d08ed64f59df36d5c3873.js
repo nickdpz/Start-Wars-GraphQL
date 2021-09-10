bb76f08553fd47ea783b85fc87b7df8659b53e4f
nickdpz <ndpastranzamora@gmail.com>
2021-09-10 02:18:53 -0500

    Updates

---
+++ precache-manifest.edd88c3d762d08ed64f59df36d5c3873.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce293e329574b2978c4e640e08864a6",
+    "url": "/Rick-Morty-GraphQL/index.html"
+  },
+  {
+    "revision": "93970725f4f90ee5b102",
+    "url": "/Rick-Morty-GraphQL/static/css/2.1816a92f.chunk.css"
+  },
+  {
+    "revision": "652fd3749b17c56e335b",
+    "url": "/Rick-Morty-GraphQL/static/css/main.ab0fbdd2.chunk.css"
+  },
+  {
+    "revision": "93970725f4f90ee5b102",
+    "url": "/Rick-Morty-GraphQL/static/js/2.47169913.chunk.js"
+  },
+  {
+    "revision": "63cbfaa305b93d86f68694a13e140c2a",
+    "url": "/Rick-Morty-GraphQL/static/js/2.47169913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652fd3749b17c56e335b",
+    "url": "/Rick-Morty-GraphQL/static/js/main.84759b54.chunk.js"
+  },
+  {
+    "revision": "529675c35a429fc2bf7c",
+    "url": "/Rick-Morty-GraphQL/static/js/runtime-main.79dfb7c3.js"
+  }
+]);

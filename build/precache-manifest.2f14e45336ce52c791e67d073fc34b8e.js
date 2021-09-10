d8db588c234a8d5b18f5f562208bbbf4249a8bb7
nickdpz <ndpastranzamora@gmail.com>
2021-09-10 01:46:43 -0500

    :pencil: 0: Readme fix

---
+++ build/precache-manifest.2f14e45336ce52c791e67d073fc34b8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf5e5e36c764d1e398ac6a2539e8465",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93970725f4f90ee5b102",
+    "url": "/static/css/2.1816a92f.chunk.css"
+  },
+  {
+    "revision": "652fd3749b17c56e335b",
+    "url": "/static/css/main.ab0fbdd2.chunk.css"
+  },
+  {
+    "revision": "93970725f4f90ee5b102",
+    "url": "/static/js/2.47169913.chunk.js"
+  },
+  {
+    "revision": "63cbfaa305b93d86f68694a13e140c2a",
+    "url": "/static/js/2.47169913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652fd3749b17c56e335b",
+    "url": "/static/js/main.84759b54.chunk.js"
+  },
+  {
+    "revision": "a12449e374b8e47f5cc7",
+    "url": "/static/js/runtime-main.31c6634a.js"
+  }
+]);
